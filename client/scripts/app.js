2f7c585f77c4314f032c87de647a2f9c88adaf2d
selvan89 <selvanprograms@gmail.com>
2014-12-30 11:41:06 -0800

    Create room list, save object of friends

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
   var $retrieveButton = $('#retrieveButton');
   var $newRoom = $('#newRoom');
   var message = $message.val();
+  var friends = {};
 
   app.clearMessages = function() {
     $('div').remove('.singleMessage');
@@ -30,6 +31,20 @@ $(document).ready(function(){
     app.addRoom($newRoom.val());
   });
 
+//befriend function
+  app.befriend = function(friend){
+    if(!friends[friend]){
+      friends[friend] = true;
+    }
+  }
+
+//befriend click event
+$('#chats').on('click', '.userText', function(){
+  // console.log("this.val()", $newRoom.val());
+  app.befriend($(this).text());
+  console.log(friends);
+});
+
 //What's the best way to separate out addMessage and fetch
   app.fetch = function(){
     app.clearMessages();
@@ -38,7 +53,7 @@ $(document).ready(function(){
       /*?order=-createdAt',*/
       type: 'GET',
       success: function(data) {
-        // console.log('data.results[99]', data.results[99]);
+        console.log('data.results[99]', data.results[99]);
         for (var i = data.results.length - 1; i > data.results.length - 21; i--) {
           var msgObj = data.results[i];
           var user = msgObj.username;
@@ -46,8 +61,13 @@ $(document).ready(function(){
           var filter = /^(\s|\w|\d|<br\/>)*?$/;
           msgText = filter.exec(msgText);
           user = filter.exec(user);
-          if (msgText !== null && user !== null && msgText[0] !== 'undefined') {
-            $chats.append('<div class="singleMessage">' + msgText[0] + ' : ' + user[0] + '</div>');
+          //check if user is in friends list
+          if (friends[user]) {
+            if (msgText !== null && user !== null && msgText[0] !== 'undefined') {
+              $chats.append('<div class="singleMessage"><em>' + '<span class="userText">' + user[0] + '</span>' + ' : ' + msgText[0] + '</em></div>');
+            }
+          } else if (msgText !== null && user !== null && msgText[0] !== 'undefined') {
+            $chats.append('<div class="singleMessage">' +'<span class="userText">' + user[0] + '</span>' + ' : ' + msgText[0] + '</div>');
           }
         }
       },
@@ -57,11 +77,67 @@ $(document).ready(function(){
     });
   };
 
+  app.fetchRooms = function(){
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      success: function(data) {
+        var uniqRoom = {};
+        for (var i = 0; i < data.results.length; i++) {
+          var room = data.results[i].roomname;
+          var filter = /^(\s|\w|\d|<br\/>)*?$/;
+          room = filter.exec(room)[0];
+          if (room !== null && room !== 'undefined') {
+            if (!uniqRoom[room]){
+              uniqRoom[room] = room;
+            }
+          }
+        }
+        // console.log('uniqRoom', uniqRoom);
+        for (var key in uniqRoom) {
+          $('#roomList').append('<div class="singleRoom">' + key + '</div>');
+        }
+      }
+    });
+  };
+
+
+  app.toggleRoom = function(newRoom) {
+    app.clearMessages();
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      success: function(data) {
+        for (var i = 0; i < data.results.length; i++) {
+          var msgObj = data.results[i];
+          var user = msgObj.username;
+          var msgText = msgObj.text;
+          var filter = /^(\s|\w|\d|<br\/>)*?$/;
+          msgText = filter.exec(msgText);
+          var room = msgObj.roomname
+          user = filter.exec(user);
+          if (msgText !== null && user !== null && msgText[0] !== 'undefined' && room === newRoom && msgText[0].length !== 0) {
+            $chats.append('<div class="singleMessage">' + msgText[0] + ' : ' + user[0] + '</div>');
+          }
+        }
+      }
+    });
+  }
+
+  $('#roomList').on('click', '.singleRoom', function(){
+    var room = $(this).text();
+    app.toggleRoom(room);
+    // console.log('room', room);
+  });
+
   $retrieveButton.on('click', function(){
+    $('div').remove('.singleRoom');
     app.fetch();
+    app.fetchRooms();
   });
 
   app.send = function(message) {
+    console.log('message', message);
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
@@ -78,7 +154,7 @@ $(document).ready(function(){
   };
 
   $submitButton.on('click', function(){
-    app.send();
+    app.send(message);
   });
 
 });
